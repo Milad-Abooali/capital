125906e820aa78f0d245b0b23e15169606830c83
Milad Abooali <m.abooali@hotmail.com>
2021-08-08 13:35:59 +0430

    UI

--- ui/default/home.php
+++ ui/default/home.php
@@ -13,6 +13,17 @@
     }
 ?>
 
+<?php
+try {
+    $path = self::UI_PATH.'global/sidebat.php';
+    (file_exists($path))
+        ? require_once $path
+        : throw new Exception("<strong style='color: #ef661b'>$path</strong> is not exist.");
+} catch (Exception $e) {
+    die($e->getMessage());
+}
+?>
+
 home.
 
 <?php Mahan4\m::print($this->Page_DATA->test) ?>
