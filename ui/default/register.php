00406a491554e5077b8d2bb9da94b3a1e2372757
Milad Abooali <m.abooali@hotmail.com>
2021-08-21 16:18:54 +0430

    Register - UI

--- ui/default/register.php
+++ ui/default/register.php
@@ -1,50 +1,4 @@
 
-<!--
-=========================================================
-* Soft UI Dashboard - v1.0.3
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://www.creative-tim.com/license)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
--->
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="apple-touch-icon" sizes="76x76" href="<?= ASSETS_PATH ?>img/apple-icon.png">
-    <link rel="icon" type="image/png" href="<?= ASSETS_PATH ?>img/favicon.png">
-    <title>
-        Soft UI Dashboard by Creative Tim
-    </title>
-    <!--     Fonts and icons     -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
-    <!-- Nucleo Icons -->
-    <link href="<?= ASSETS_PATH ?>css/nucleo-icons.css" rel="stylesheet" />
-    <link href="<?= ASSETS_PATH ?>css/nucleo-svg.css" rel="stylesheet" />
-    <!-- Font Awesome Icons -->
-    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-    <link href="<?= ASSETS_PATH ?>css/nucleo-svg.css" rel="stylesheet" />
-    <!-- CSS Files -->
-    <link id="pagestyle" href="<?= ASSETS_PATH ?>css/soft-ui-dashboard.css?v=1.0.3" rel="stylesheet" />
-</head>
-
-<body class="">
-<div class="container position-sticky z-index-sticky top-0">
-    <div class="row">
-        <div class="col-12">
-
-        </div>
-    </div>
-</div>
 <main class="main-content  mt-0">
     <section>
         <div class="page-header min-vh-75">
@@ -114,7 +68,7 @@
 
 <?php
 try {
-    $path = self::UI_PATH.'global/gust_footer.php';
+    $path = self::UI_PATH.'global/guest_footer.php';
     (file_exists($path))
         ? require_once $path
         : throw new Exception("<strong style='color: #ef661b'>$path</strong> is not exist.");
