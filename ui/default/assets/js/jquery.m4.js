11146a72bce12ebd857af1381bef13d44844d6f2
Milad Abooali <m.abooali@hotmail.com>
2021-08-22 15:11:03 +0430

    Register - UI

--- ui/default/assets/js/jquery.m4.js
+++ ui/default/assets/js/jquery.m4.js
@@ -1,9 +1,18 @@
 /**
  * Ajax Call
  */
-(function( $ ){
-    $.fn.m4_Ajax = function() {
-        alert('hello world');
+(function ( $ ) {
+    $.fn.m4_Ajax = function( options ) {
+        var settings = $.extend({
+            color: "#556b2f",
+            backgroundColor: "white"
+        }, options );
+        this.each(function() {
+            this.css({
+                color: settings.color,
+                backgroundColor: settings.backgroundColor
+            });
+        });
         return this;
     };
-})(jQuery);
+}( jQuery ));
