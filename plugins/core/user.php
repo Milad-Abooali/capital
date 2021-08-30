daab00914b6a7431e798f690239ec38de0250ab0
Milad Abooali <m.abooali@hotmail.com>
2021-08-30 04:44:49 +0430

    LIB - user

--- plugins/core/user.php
+++ plugins/core/user.php
@@ -20,6 +20,7 @@
 
     namespace Mahan4\Plugins;
 
+    use cscList\cscList;
     use Mahan4\sanitize;
     use Mosquitto\Exception;
 
@@ -35,7 +36,7 @@
          */
         public static function email(mixed $value) :string
         {
-            if(self::isEmail($value))
+            if(self::isEmail($value) && (strlen($value)>4))
                 return strtolower($value);
             throw new Exception('Email address is not valid!');
         }
@@ -46,7 +47,7 @@
          */
         public static function f_name(mixed $value) :string
         {
-            if(preg_match("/^([a-zA-Z' ]+)$/", $value))
+            if(preg_match("/^([a-zA-Z' ]+)$/", $value) && (strlen($value)>2))
                 return ucfirst($value);
             throw new Exception('First Name is not valid!');
         }
@@ -57,9 +58,32 @@
          */
         public static function l_name(mixed $value) :string
         {
-            if(preg_match("/^([a-zA-Z' ]+)$/", $value))
+            if(preg_match("/^([a-zA-Z' ]+)$/", $value) && (strlen($value)>2))
                 return ucfirst($value);
             throw new Exception('First Name is not valid!');
+        }
+
+        /**
+         * Country
+         * @throws Exception
+         */
+        public static function country(mixed $value) :string
+        {
+            $cscList = new cscList();
+            if($cscList->isCountry($value))
+                return $value;
+            throw new Exception('Country is not contain in our list!');
+        }
+
+        /**
+         * City
+         * @throws Exception
+         */
+        public static function city(mixed $value) :string
+        {
+            if(preg_match("/^([a-zA-Z' ]+)$/", $value) && (strlen($value)>2))
+                return $value;
+            throw new Exception('City Name is not valid!');
         }
 
     }
