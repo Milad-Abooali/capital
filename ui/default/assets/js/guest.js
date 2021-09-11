bba8ce4ffdd2ef5733df1a3d968b1afea223d833
Milad Abooali <m.abooali@hotmail.com>
2021-09-11 19:26:09 +0430

    User - Regiser

--- ui/default/assets/js/guest.js
+++ ui/default/assets/js/guest.js
@@ -11,7 +11,27 @@
             type:'form'
         };
         $(this).m4_Ajax(ajaxOptions, function(call){
-            if(call.res);
+            if(call.res) {
+                toastr.options = {
+                    "closeButton": false,
+                    "debug": false,
+                    "newestOnTop": false,
+                    "progressBar": false,
+                    "positionClass": "toast-bottom-right",
+                    "preventDuplicates": false,
+                    "onclick": null,
+                    "showDuration": "300",
+                    "hideDuration": "1000",
+                    "timeOut": "5000",
+                    "extendedTimeOut": "1000",
+                    "showEasing": "swing",
+                    "hideEasing": "linear",
+                    "showMethod": "fadeIn",
+                    "hideMethod": "fadeOut"
+                }
+                toastr["error"](call.res.e)
+
+            }
         });
     });
 
